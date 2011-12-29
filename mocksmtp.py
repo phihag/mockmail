a1e5b1facc06e8918eda868cb90ce6e6165176fe
Philipp Hagemeister <phihag@phihag.de>
2011-12-30 00:25:18 +0100

    First basic version (still doesn't display body and subject)

---
+++ mocksmtp.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import BaseHTTPServer
+import asyncore
+import datetime
+import grp
+import json
+import mimetypes
+import os
+import pwd
+import smtpd
+import tempfile
+import threading
+from optparse import OptionParser
+
+import pystache # If this fails, execute $ pip install pystache
+
+
+def _readfile(fn):
+	with open(fn, 'rb') as f:
+		return f.read()
+
+_BASE_DIR = os.path.dirname(__file__)
+
+class _OnDemandIdReader(object):
+	def __init__(self, ids, fnCalc, mapContent):
+		self._ids = ids
+		self._fnCalc = fnCalc
+		self._mapContent = mapContent
+	def __getitem__(self, key):
+		if key not in self._ids:
+			raise KeyError()
+		res = _readfile(self._fnCalc(key))
+		if self._mapContent:
+			res = self._mapContent(res)
+		return res
+	def __len__(self):
+		return len(self._ids)
+	def __contains__(self, item):
+		return item in self._ids
+
+def _readIds(ids, fnCalc, mapContent=None, ondemand=False):
+	""" Read all the files calculated by map(fnCalc, ids), and return a dictionary {id: mapContent(file content)}
+	@param ondemand If this is set, do not actually return a dictionary, but a mock object that reads the contents everytime it is accessed.
+	"""
+
+	if ondemand:
+		return _OnDemandIdReader(ids, fnCalc, mapContent)
+	else:
+		if mapContent is None:
+			mapContent = lambda x:x
+		return dict((fid, mapContent(_readfile(fnCalc(fid)))) for fid in ids)
+
+class MailStore(object):
+	""" Threadsafe mail storage class """
+	def __init__(self):
+		self._lock = threading.Lock()
+		self._mails = []
+
+	def add(self, mail):
+		self._lock.acquire()
+		self._mails.append(mail)
+		self._lock.release()
+
+	@property
+	def mails(self):
+		self._lock.acquire()
+		try:
+			return self._mails[:]
+		finally:
+			self._lock.release()
+
+	@property
+	def delete(self, filterf=None):
+		self._lock.acquire()
+		try:
+			self._mails = filter(filterf, self._mails)
+		finally:
+			self._lock.release()
+
+class MockSmtpServer(smtpd.SMTPServer):
+	def __init__(self, localaddr, port, ms):
+		self._ms = ms
+		smtpd.SMTPServer.__init__(self, (localaddr, port), None)
+
+	def process_message(self, peer, mailfrom, rcpttos, data):
+		mail = {
+			'peer_ip': peer[0],
+			'peer_port': peer[1],
+			'from': mailfrom,
+			'to': rcpttos[0] if len(rcpttos) > 0 else '<nobody>',
+			'rawdata': data,
+			'subject': 'TODO',
+		}
+		self._ms.add(mail)
+
+class MocksmtpHttpServer(BaseHTTPServer.HTTPServer):
+	def __init__(self, localaddr, port, ms, httpTemplates, staticFiles):
+		self.ms = ms
+		self.httpTemplates = httpTemplates
+		self.staticFiles = staticFiles
+		BaseHTTPServer.HTTPServer.__init__(self, (localaddr, port), _MocksmtpHttpRequestHandler)
+
+
+class _MocksmtpHttpRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+	def _serve_template(self, tname, context):
+		templates = self.server.httpTemplates
+		try:
+			body = pystache.render(templates[tname], context)
+			page = pystache.render(templates['root'], {'title': context['title'], 'body': body})
+			pageBlob = page.encode('utf-8')
+		except:
+			self.send_error(500)
+			raise
+
+		self.send_response(200)
+		self.send_header('Content-Type', 'text/html; charset=utf-8')
+		self.end_headers()
+		self.wfile.write(pageBlob)
+
+	def do_GET(self):
+		if self.path == '/':
+			mails = self.server.ms.mails
+			for i,m in enumerate(mails):
+				m['id'] = str(i)
+			self._serve_template('index', {'emails': mails, 'title': 'mocksmtpserver'})
+		elif self.path.startswith('/mails/'):
+			mailid_str = self.path[len('/mails/'):]
+			try:
+				mail = self.server.ms.mails[int(mailid_str)]
+			except (KeyError, IndexError, ValueError):
+				self.send_error(404)
+				return
+			maildict = mail.copy()
+			maildict['title'] = maildict['subject'] + ' - mocksmtp'
+			self._serve_template('mail', maildict)
+		elif self.path.startswith('/static/'):
+			fn = self.path[len('/static/'):]
+			if fn not in self.server.staticFiles:
+				self.send_error(404)
+				return
+			content = self.server.staticFiles[fn]
+			contentType = mimetypes.guess_type(fn)[0]
+
+			self.send_response(200)
+			self.send_header('Content-Type', contentType)
+			self.send_header('Content-Length', str(len(content)))
+			self.end_headers()
+			self.wfile.write(content)
+		else:
+			self.send_error(404)
+
+	def log_request(self, code='-', size='-'):
+		pass
+	def log_error(*args, **kwargs):
+		pass
+
+def _dropPrivileges(config):
+	uid = None
+	if config['dropuser'] is not None:
+		uname = config['dropuser']
+		gname = config['dropgroup']
+
+		if isinstance(uname, int):
+			uid = uname
+			if gname is None:
+				gname = pwd.getpwuid(uid).pw_gid
+		else:
+			pw = pwd.getpwnam(uname)
+			uid = pw.pw_uid
+			if gname is None:
+				gname = pw.pw_gid
+
+		if isinstance(gname, int):
+			gid = gname
+		else:
+			gid = grp.getgrnam(gname).gr_gid
+
+	if config['chroot']:
+		if config['chroot'] == True: # chroot into a temporary directory
+			chrootDir = tempfile.mkdtemp('mocksmtp')
+			try:
+				os.chroot(chrootDir)
+			except OSError:
+				os.rmdir(chrootDir)
+				raise
+		else: # chroot into a regular directory
+			os.chroot(config['chroot'])
+		os.chdir('/')
+
+	if uid is not None:
+		os.setgroups([])
+		os.setgid(gid)
+		os.setuid(uid)
+
+def mockSMTP(config):
+	ms = MailStore()
+
+	smtpSrv = MockSmtpServer(config['smtpaddr'], config['smtpport'], ms)
+
+	httpTemplates = _readIds(
+		('root', 'index', 'mail'),
+		lambda fid: os.path.join(_BASE_DIR, 'templates', fid + '.mustache'),
+		mapContent=lambda content:content.decode('UTF-8'),
+		ondemand=config['static_dev'])
+	httpStatic = _readIds(
+		('mocksmtp.css',),
+		lambda fid: os.path.join(_BASE_DIR, 'static', fid),
+		ondemand=config['static_dev'])
+	httpSrv = MocksmtpHttpServer(config['httpaddr'], config['httpport'], ms, httpTemplates, httpStatic)
+
+	_dropPrivileges(config)
+
+	smtpThread = threading.Thread(target=asyncore.loop)
+	smtpThread.daemon = True
+	smtpThread.start()
+
+	httpThread = threading.Thread(target=httpSrv.serve_forever)
+	httpThread.daemon = True
+	httpThread.start()
+
+	smtpThread.join()
+
+def main():
+	parser = OptionParser()
+	parser.add_option('-c', '--config', dest='configfile', metavar='FILE')
+	opts,args = parser.parse_args()
+
+	if len(args) != 0:
+		parser.error('Did not expect any arguments. Use -c to specify a configuration file.')
+
+	config = {
+		'smtpaddr': '', # IP address to bind the SMTP port on
+		'smtpport': 25, # SMTP port number. On unixoid systems, you will need superuser privileges to bind to a port < 1024
+		'httpaddr': '',
+		'httpport': 2580,
+		'chroot': False,     # Set to True to chroot into a temporary directory. Alternatively, specify the directory to chroot into.
+		'dropuser': None,    # User account (name or uid) to drop to, None to not drop privileges
+		'dropgroup': None,   # User group (name or gid) to drop into. By default, this is the primary group of the user.
+		'static_dev': False, # Read static files on demand. Good for development (a reload will update the file), but should not be set in production
+	}
+	if opts.configfile:
+		with open(opts.configfile , 'r') as cfgf:
+			config.update(json.load(cfgf))
+
+	mockSMTP(config)
+
+if __name__ == '__main__':
+	main()
